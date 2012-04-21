9dd873676dbb37e45e59c959fd8e134ce043f764
Avelar Leao <awleao@gmail.com>
2012-04-21 13:19:23 -0300

    Revert "inclusao dos arquivos da entidade de relacionamento de follow"
    This reverts commit 56e113be0e46803c28a68c1ac45ae1e422a46654.
    
    Conflicts:
    
    	src/main/java/br/com/fa7/twitter/entities/UserFollow.java

--- src/main/java/br/com/fa7/twitter/business/dao/UserFollowDAO.java
+++
@@ -1,9 +0,0 @@
-package br.com.fa7.twitter.business.dao;
-
-import org.springframework.stereotype.Component;
-
-import br.com.fa7.twitter.business.dao.util.HibernateDAOGenerico;
-import br.com.fa7.twitter.entities.UserFollow;
-
-@Component
-public class UserFollowDAO extends HibernateDAOGenerico<UserFollow, Long> {}
