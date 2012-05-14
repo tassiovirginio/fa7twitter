ee9498cca5bdd4d010c8c4742626339cb922de96
Tassio Virginio <tassiovirginio@gmail.com>
2012-05-14 20:13:06 -0300

    Testes Funcionais (Login e Envio de Msg.)

--- src/test/java/br/com/fa7/twitter/selenium/Teste.java
+++ src/test/java/br/com/fa7/twitter/selenium/Teste.java
@@ -5,10 +5,13 @@ import java.util.concurrent.TimeUnit;
 import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.BeforeClass;
-import org.junit.Ignore;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.springframework.ui.context.Theme;
+
 
 public class Teste {
 
@@ -16,37 +19,65 @@ public class Teste {
 	private static Login pageLogin;
 	private static String janelaPrincipal;
 	
+	
 	@BeforeClass
 	public static void setUp() {
 		
-		driver = new FirefoxDriver();		
-		
+		driver = new FirefoxDriver();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-		
 		driver.get("http://127.0.0.1:9999");		
-		
 		janelaPrincipal = driver.getWindowHandle();
-		
 		pageLogin = new Login(driver);
 		
 	}
 	
 	@AfterClass
 	public static void setDown(){
-		
 		for (String handle : driver.getWindowHandles()) {
-			
 		    driver.switchTo().window(handle).close();
-		    
 		}
 
 	}
 
-	@Ignore
+//	@Ignore
+	@Test
 	public void loginSucesso() throws InterruptedException{		
 
-		// PASSO 1		
+		WebElement queryLogin = driver.findElement(By.name("login"));
+		queryLogin.sendKeys("tassio");
+		Thread.sleep(1000);
+        WebElement querySenha = driver.findElement(By.name("senha"));
+        querySenha.sendKeys("123");
+        Thread.sleep(1000);
+        querySenha.submit(); 
+        Thread.sleep(1000);
+		Assert.assertTrue(true);
+	}
+	
+	
+	@Test
+	public void enviarMsg() throws InterruptedException{		
+
+		WebElement queryMsg = driver.findElement(By.name("taMsg"));
+		queryMsg.sendKeys("Teste");
+		Thread.sleep(1000);
+		queryMsg.submit();
 		
+		Assert.assertTrue(true);
+		Thread.sleep(4000);
+	}
+	
+//	@Test
+	public void navegarNoMenu() throws InterruptedException{		
+
+		WebElement queryHome = driver.findElement(By.name("Home"));
+		queryHome.submit();
+		
+		Thread.sleep(3000);
+		
+		WebElement queryBuscar = driver.findElement(By.name("Buscar"));
+		queryBuscar.submit();
+		Thread.sleep(3000);
 		Assert.assertTrue(true);
 	}
 
